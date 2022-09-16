87236066663a204133049158484bf6f43adaa544
Mauro Ruiz <mauroruiz146@gmail.com>
2022-09-16 15:04:25 -0500

    Commit 3

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
-import { useTheme, Text, Spacer, Image } from "@nextui-org/react";
+import { useTheme, Text, Spacer, Image, Link } from "@nextui-org/react";
+import NextLink from 'next/link';
 
 export const Navbar = () => {
 
@@ -22,12 +23,20 @@ export const Navbar = () => {
                 height={70}
             />
 
-            <Text color="white" h2>P</Text>
-            <Text color="white" h3>okemon</Text>
+            <NextLink href='/' passHref>
+                <Link>
+                    <Text color="white" h2>P</Text>
+                    <Text color="white" h3>okemon</Text>
+                </Link>
+            </NextLink>
 
             <Spacer css={{ flex: 1 }}/>
 
-            <Text color="white">Favoritos</Text>
+            <NextLink href='/favorites' passHref>
+                <Link>
+                    <Text color="white">Favoritos</Text>
+                </Link>
+            </NextLink>
         </div>
     )
 }
